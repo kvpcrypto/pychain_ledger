d191e6233bb3d61aff81cac966e8f1ca2d8a3c56
kenny <pham.kenny3@gmail.com>
2022-12-01 00:34:00 -0500

    Completed file and screenshot of app

--- pychain.py
+++ pychain.py
@@ -49,7 +49,11 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
+@dataclass
+class Record:
+    sender: str
+    receiver: str
+    amount: float
 
 
 ################################################################################
@@ -68,7 +72,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+    record: Record
 
     creator_id: int
     prev_hash: str = "0"
@@ -87,11 +91,13 @@ class Block:
         timestamp = str(self.timestamp).encode()
         sha.update(timestamp)
 
-        prev_hash = str(self.prev_hash).encode()
-        sha.update(prev_hash)
+        
 
         nonce = str(self.nonce).encode()
         sha.update(nonce)
+
+        prev_hash = str(self.prev_hash).encode()
+        sha.update(prev_hash)
 
         return sha.hexdigest()
 
@@ -166,19 +172,19 @@ pychain = setup()
 
 # @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
+#st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender_id = st.text_input("Sender ID")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver_id = st.text_input("Receiver ID")
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+value = st.number_input("Amount")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -189,7 +195,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        record=Record(sender_id, receiver_id, value),
         creator_id=42,
         prev_hash=prev_block_hash
     )
